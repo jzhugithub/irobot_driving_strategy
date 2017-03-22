0a988b4fc641e98e977a1e5ea07939562059b808
jzhugithub <mfx106825372@163.com>
2017-03-22 23:00:54 +0800

    add flag for collision

--- data2/touchJudgement.m
+++ data2/touchJudgement.m
@@ -7,8 +7,12 @@ s_g_head.z = s_g.z;
 
 if (s_f.z-s_g_head.z)<0.1 && norm([s_f.x-s_g_head.x s_f.y-s_g_head.y])<0.2
     touchType = 2;
+    figure(1)
+%     plot(s_f.x,s_f.y,'.r','MarkerSize',8);
 elseif s_f.topTouchFlag == 1  && s_f.z<0.25 && norm([s_f.x-s_g_head.x s_f.y-s_g_head.y])<0.2
     touchType = 1;
+    figure(1)
+%     plot(s_f.x,s_f.y,'.g','MarkerSize',8);
 else
     touchType = 0;
 end
