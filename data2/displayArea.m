6cf7fb56dc9903986f373d69b47a1c591d221504
jzhugithub <mfx106825372@163.com>
2017-03-22 22:59:57 +0800

    fix color bug on matlab2016

--- data2/displayArea.m
+++ data2/displayArea.m
@@ -12,23 +12,22 @@ else
     figure(1)
 %     set(gcf,'color','w');
     %% x-y view
-    subplot(1,2,1);
     box on;
     hold on;
     for s_gNum = 1:size(s_g_mult,2)
-        iRobotTrace(s_gNum) = plot(s_g_mult(s_gNum).x,s_g_mult(s_gNum).y,s_g_mult(s_gNum).color,'MarkerSize',3);
+        iRobotTrace(s_gNum) = plot(s_g_mult(s_gNum).x,s_g_mult(s_gNum).y,'.','Color',s_g_mult(s_gNum).color,'MarkerSize',3);
         iRobot(s_gNum) = plot(s_g_mult(s_gNum).x,s_g_mult(s_gNum).y,'ok','MarkerSize',8);
     end
 
     TariRobot = plot(s_g_mult(s_gTarNum).x,s_g_mult(s_gTarNum).y,'or','MarkerSize',8);
-    quadrotor = plot(s_f.x,s_f.y,'*g','MarkerSize',8);
+    quadrotor = plot(s_f.x,s_f.y,'.k','MarkerSize',16);
 %     quadrotorTrace = plot(s_f.x,s_f.y,'.g','MarkerSize',3);%%%%%%%%%%% for debug
     [firTurn,secTurn] = computeEnd(s_g_mult(s_gTarNum),Time);
     turn1 = plot(firTurn.x,firTurn.y,'*b');
     turn2 = plot(secTurn.x,secTurn.y,'*r');
     axis([0,20,0,20]);
     %% z view
-    subplot(1,2,2);
+    figure(2)
     box on;
     grid on;
     hold on;
@@ -38,6 +37,7 @@ else
     pause(delay);
     set([iRobot,TariRobot,quadrotor,iRobotZ,turn1,turn2],'visible','off');
 %     delete(iRobot,TariRobot,quadrotor,iRobotZ,turn1,turn2);
+%     set([TariRobot,iRobotZ],'visible','off');%%%%%%%%%%% for debug
 %     delete(TariRobot,iRobotZ);%%%%%%%%%%% for debug
 end
 end
