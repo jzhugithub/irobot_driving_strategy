f8a041ec4330645a398fa1515111ce386defe813
jzhugithub <mfx106825372@163.com>
2017-03-22 23:02:05 +0800

    add save area, increase angle

--- data2/stateDiscretize.m
+++ data2/stateDiscretize.m
@@ -1,21 +1,22 @@
 function discState = stateDiscretize(stateDim,s_f,s_g,Time)
 %% theta
 s_g.theta = mod(s_g.theta,2*pi);
-discState.theta = floor((s_g.theta+pi*3/8) / (pi/4));
-if discState.theta>8
-    discState.theta = 1;
-end
+% discState.theta = floor((s_g.theta+pi*3/8) / (pi/4));
+% if discState.theta>8
+%     discState.theta = 1;
+% end
+discState.theta = min(floor(s_g.theta/(pi/8))+1,16);
 %% t
 Time = mod(Time,20);
 discState.t = max([min([ceil(Time/2),10]),1]);
 % max([1,floor((Time+1.99)/2)]);
 %% d 
 discState.d = 1;
-% if s_g.x>18
-%     discState.d = 2;
-% elseif s_g.x<2
-%     discState.d = 3;
-% end
+if s_g.x>18
+    discState.d = 2;
+elseif s_g.x<2
+    discState.d = 3;
+end
 %% l
 discState.l = ceil(norm([s_g.x-s_f.x s_g.y-s_f.y]));
 discState.l = max([min([discState.l,5]),1]);
