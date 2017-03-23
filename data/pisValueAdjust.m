6fae4839372e37a3a64d152f4db97de20b2f15cf
jzhugithub <mfx106825372@163.com>
2017-03-23 17:48:01 +0800

    delete data

--- data/pisValueAdjust.m
+++
@@ -1,9 +0,0 @@
-function aPisValue = pisValueAdjust(pisValue,distancef2g)
-% adjust s_g's pisValue whose distance to quadrotor if bigger than 5
-if distancef2g>5
-    aPisValue = pisValue - distancef2g * 10;
-else
-    aPisValue = pisValue;
-end
-
-end
